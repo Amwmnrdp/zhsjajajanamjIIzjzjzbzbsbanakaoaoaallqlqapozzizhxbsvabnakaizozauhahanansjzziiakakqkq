97bfa4d65612c2cb11b26c34ceb12fba6a6bacf7
Amwmnrdp <mwmnff1122@gmail.com>
2025-12-27 15:08:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -969,9 +969,6 @@ app.post('/api/set-owner', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🌐 Web server running on port ${PORT}`);
-});
+
 
 client.login(process.env.token);
